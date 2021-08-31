55dca8cd8a9d3a2f95d23adfba857b86a90faa48
Josh Bendavid <Josh.Bendavid@cern.ch>
2021-08-31 14:02:33 +0200

    skip empty files by default and add option to specify xrootd server

--- pllxrdcp.py
+++ pllxrdcp.py
@@ -7,12 +7,14 @@ from concurrent.futures import ThreadPoolExecutor
 parser = argparse.ArgumentParser()
 parser.add_argument("-j","--jobs", type=int, default=32)
 parser.add_argument("-r","--recursive", action='store_true')
+parser.add_argument("-e", "--empty", action='store_true')
+parser.add_argument("-s", "--server", type=str, default = "eoscms.cern.ch")
 parser.add_argument("source", type=str, nargs=1)
 parser.add_argument("dest", type=str, nargs=1)
 
 args = parser.parse_args();
 
-cmds = ["xrdfs", "eoscms.cern.ch", "ls"]
+cmds = ["xrdfs", args.server, "ls", "-l"]
 
 if args.recursive:
     cmds += ["-R"]
@@ -26,12 +28,19 @@ res.check_returncode()
 
 lsfiles = str(res.stdout, 'utf-8').splitlines()
 
+lsfilenames = []
+for f in lsfiles:
+    fsplit = f.split(" ")
+    filesize = fsplit[-2]
+    filename = fsplit[-1]
+    if args.empty or filesize != "0":
+        lsfilenames.append(filename)
+
 basedir = args.source[0].split("/")[:-1]
 basedir = "/".join(basedir)
-#print(basedir)
 
-infiles = [f"root://eoscms.cern.ch/{f}" for f in lsfiles]
-outfiles = [f.replace(basedir, args.dest[0]) for f in lsfiles]
+infiles = [f"root://eoscms.cern.ch/{f}" for f in lsfilenames]
+outfiles = [f.replace(basedir, args.dest[0]) for f in lsfilenames]
 
 def xrdcp(files):
     subprocess.run(["xrdcp", files[0], files[1]])
