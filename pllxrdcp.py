4bb6969e1c829aaa81e264c9b7f13d85fe071652
Kenneth Long <kdlong@hep.wisc.edu>
2022-11-09 13:42:29 +0100

    Add dry run option

--- pllxrdcp.py
+++ pllxrdcp.py
@@ -10,6 +10,7 @@ parser.add_argument("-r","--recursive", action='store_true')
 parser.add_argument("-e", "--empty", action='store_true')
 parser.add_argument("-s", "--server", type=str, default = "eoscms.cern.ch")
 parser.add_argument("--maxFiles", type=int, default=None)
+parser.add_argument("--dryRun", action='store_true', help="Print command but don't copy")
 parser.add_argument("source", type=str, nargs=1)
 parser.add_argument("dest", type=str, nargs=1)
 
@@ -20,9 +21,10 @@ cmds = ["xrdfs", args.server, "ls", "-l"]
 if args.recursive:
     cmds += ["-R"]
     
-cmds += args.source
+cmds.append(args.source)
     
-print(cmds)
+print("Command to find all files:", cmds)
+
 res = subprocess.run(cmds, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 res.check_returncode()
 
@@ -39,11 +41,16 @@ if args.maxFiles and args.maxFiles < len(lsfilenames):
     print(f"INFO: copying the first {args.maxFiles} of {len(lsfilenames)} valid files")
     lsfilenames = lsfilenames[:args.maxFiles]
 
-basedir = args.source[0].split("/")[:-1]
+basedir = args.source.rstrip("/").split("/")[:-1]
 basedir = "/".join(basedir)
 
-infiles = [f"root://{args.server}/{f}" for f in lsfilenames]
-outfiles = [f.replace(basedir, args.dest[0]) for f in lsfilenames]
+infiles = [f"root://eoscms.cern.ch/{f}" for f in lsfilenames]
+outfiles = [f.replace(basedir, args.dest) for f in lsfilenames]
+
+if args.dryRun:
+    print(f"Will run the following command on {args.jobs} threads (example for the first file):")
+    print(f"--> xrdcp {infiles[0]} {outfiles[0]}")
+    exit(0)
 
 def xrdcp(files):
     subprocess.run(["xrdcp", files[0], files[1]])
